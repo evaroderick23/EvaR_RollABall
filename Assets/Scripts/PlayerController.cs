6fcec2d97dc642ed19a19d40c56dac59d7f562c9
evaroderick23 <evaroderick23@gmail.com>
2024-10-17 22:02:57 -0300

    Added a count, controls and a win screen to lvl 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,7 +38,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        if(count >= 6)
         {
             winTextObject.SetActive(true);
         }
