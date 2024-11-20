3a01f7551e6434f64ce261eedf27729c58544f81
evaroderick23 <evaroderick23@gmail.com>
2024-11-20 09:38:18 -0400

    Added a pause menu and music

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; 
-using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-   
-        SceneManager.LoadScene("Level1"); 
+
+        SceneManager.LoadScene("Level1");
     }
 
     public void QuitGame()
     {
         Debug.Log("Exiting the game...");
-        Application.Quit(); 
+        Application.Quit();
     }
 }
